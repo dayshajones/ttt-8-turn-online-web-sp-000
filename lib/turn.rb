20d470542a175b9b879c1052e7f973c75c6b53e7
Daysha Jones <daysha.vu@icloud.com>
2021-03-10 20:14:11 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -27,7 +27,7 @@ def turn(board)
   user_input = gets.strip
   index = input_to_index(user_input)
   if valid_move?(board, index) == true
-    move(board, index, token - "X")
+    move(board, index, token = "X")
     display_board(board)
   else
     turn(board)
